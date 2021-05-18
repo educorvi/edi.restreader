20332c02ee3c1c8c515c0dd0fd2d97ae9f466094
lwalther <lwalther@novareto.de>
2021-05-18 14:18:36 +0200

    Pseudo-Code für DB-Adapter

--- edi/restreader/restaccess.py
+++ edi/restreader/restaccess.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*-
-import unicodedata
-import re
-from restclient import GET, POST, PUT, DELETE
 import requests
 import json
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-from zope.schema.interfaces import IContextSourceBinder
-from zope.interface import directlyProvides
-from plone.memoize import ram
 from time import time
-from plone import api as ploneapi
 
 login = {'login': 'restaccess', 'password': 'H9jCg768'}
 authurl = u'http://emissionsarme-produkte.bgetem.de/@login'
@@ -29,42 +21,34 @@ def getCatalogData(query):
     results = requests.get(searchurl, headers=headers, params=query)
     return results.json().get('items')
 
-@ram.cache(lambda *args: time() // (60 * 60))
-def possibleGefahrstoffe(context):
+def possibleGefahrstoffe():
     terms = []
-    #terms.append(SimpleVocabulary.createTerm(u'auswahl', u'auswahl', u'bitte auswählen'))
     payload = {'portal_type': 'nva.chemiedp.produktdatenblatt',
            'b_size': 500,
            'sort_on': 'sortable_title',
            'metadata_fields':'UID'}
     entries = getCatalogData(payload)
     for i in entries:
-        terms.append(SimpleVocabulary.createTerm(i.get('@id'), i.get('UID'), i.get('title')))
-    return SimpleVocabulary(terms)
-directlyProvides(possibleGefahrstoffe, IContextSourceBinder)
+        print(i)
 
-def getExternalGefahrstoffe():
-    payload = {'portal_type': 'nva.chemiedp.produktdatenblatt',
+def getHersteller():
+    payload = {'portal_type': 'nva.chemiedp.hersteller',
            'b_size': 500,
            'sort_on': 'sortable_title',
            'metadata_fields':'UID'}
     entries = getCatalogData(payload)
     return entries
 
-def externalGefahrstoffList():
-    terms = []
-    entries = getExternalGefahrstoffe()
-    for i in entries:
-        clearid = i.get('@id').split('/')[-1]
-        #if ploneapi.content.find(portal_type='Schutzhandschuh', Gefahrstoffe=i.get('@id')):
-        terms.append(SimpleVocabulary.createTerm(i.get('@id'), clearid, i.get('title')))
-    return terms
+if __name__ == "__main__":
+    #conn = psycopg.connect("dbname=test user=postgres")
+    #cur = conn.cursor()
+    erg = getHersteller()
+    print(erg)
+    #for i in erg:
+    #    hersteller_title = i.title
+    #    hersteller_desc = i.description
+    #    insert = "INSERT INTO manufacturer(title, description) (%s, %s)" % (hersteller_title, hersteller_desc)
+    #    cur.execute(insert)
+    #    cur.fetchall()
 
-def getExternalGefahrstoff(url):
-    token = getAuthToken()
-    headers = {
-        'Accept': 'application/json',
-        'Authorization': 'Bearer %s' % token,
-        }
-    result = requests.get(url, headers=headers)
-    return result.json() 
+    import pdb;pdb.set_trace()
