7c70e54f7e298d986d8c6a80a6227fd28da22951
Seppo Walther <seppo.walther@educorvi.de>
2021-06-21 16:25:47 +0200

    Adding printing_machine and spray_powder

--- edi/restreader/restaccess.py
+++ edi/restreader/restaccess.py
@@ -40,13 +40,32 @@ def getHersteller():
     entries = getCatalogData(payload)
     return entries
 
+def getMachines():
+    payload = {'portal_type': 'nva.chemiedp.maschine',
+           'b_size': 500,
+           'sort_on': 'sortable_title',
+           'metadata_fields':'UID'}
+    entries = getCatalogData(payload)
+    return entries
+
+def getPowders():
+    payload = {'portal_type': 'nva.chemiedp.druckbestaeubungspuder',
+           'b_size': 500,
+           'sort_on': 'sortable_title',
+           'metadata_fields':'UID'}
+    entries = getCatalogData(payload)
+    return entries
+
 if __name__ == "__main__":
     hostname = 'localhost'
-    username = 'seppowalther'
+    username = 'postgres'
     database = 'gefahrstoff'
 
     erg = getHersteller()
+    erg2 = getMachines()
+    erg3 = getPowders()
     conn = psycopg2.connect(host = hostname, user=username, dbname=database)
+    #"""
     for i in erg:
         hersteller_title = i.get('title')
         hersteller_desc = i.get('description')
@@ -54,10 +73,38 @@ if __name__ == "__main__":
         hersteller_link = i.get('@id')
         cur = conn.cursor()
         #cur.execute("INSERT INTO manufacturer (title, description, webcode) VALUES (%s, %s, %s)") % (hersteller_title, hersteller_desc, hersteller_uid)
-        cur.execute("INSERT INTO manufacturer (title, description, webcode, manufacturer_image_id) VALUES (%s, %s, %s, NULL);", (hersteller_title, hersteller_desc, hersteller_uid))
+        cur.execute("INSERT INTO manufacturer (title, description, webcode, image_id) VALUES (%s, %s, %s, NULL);", (hersteller_title, hersteller_desc, hersteller_uid))
         conn.commit()
         print(hersteller_title)# correct
         cur.close()
+    #"""
+
+    for i in erg2:
+        machine_title = i.get('title')
+        machine_desc = i.get('description')
+        machine_uid = i.get('UID')
+        machine_link = i.get('@id')
+        cur = conn.cursor()
+        # cur.execute("INSERT INTO manufacturer (title, description, webcode) VALUES (%s, %s, %s)") % (hersteller_title, hersteller_desc, hersteller_uid)
+        cur.execute("INSERT INTO printing_machine (title, description, webcode, image_url) VALUES (%s, %s, %s, NULL);",
+                    (machine_title, machine_desc, machine_uid))
+        conn.commit()
+        print(machine_title)  # correct
+        cur.close()
+
+    for i in erg3:
+        powder_title = i.get('title')
+        powder_desc = i.get('description')
+        powder_uid = i.get('UID')
+        powder_link = i.get('@id')
+        cur = conn.cursor()
+        # cur.execute("INSERT INTO manufacturer (title, description, webcode) VALUES (%s, %s, %s)") % (hersteller_title, hersteller_desc, hersteller_uid)
+        cur.execute("INSERT INTO spray_powder (title, description, webcode, image_url) VALUES (%s, %s, %s, NULL);",
+                    (powder_title, powder_desc, powder_uid))
+        conn.commit()
+        print(powder_title)  # correct
+        cur.close()
+
 
     #    insert = "INSERT INTO manufacturer(title, description) (%s, %s)" % (hersteller_title, hersteller_desc)
     #    cur.execute(insert)
